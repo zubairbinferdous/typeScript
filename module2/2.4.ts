a2b689657552a9a0bcf496673c86f94f59d25304
zubairbinferdous <zubidev71@gmail.com>
2023-12-09 23:45:35 +0600

    ok

--- module2/2.4.ts
+++ module2/2.4.ts
@@ -1,2 +1,41 @@
 {
+  // interface generic
+
+  interface Developer<T> {
+    name: string;
+    computer: {
+      brand: string;
+      model: string;
+      releaseYear: number;
+    };
+    smartWatch: T;
+  }
+
+  const user: Developer<> = {
+    name: "zubair",
+    computer: {
+      brand: "asd",
+      module: "live",
+      releaseYear: 2018,
+    },
+    smartWatch: {
+      brand: "apple",
+      model: "X-mel",
+    },
+  };
+
+  const user2: Developer<> = {
+    name: "zubair khan",
+    computer: {
+      brand: "ReadMe",
+      module: "live-009",
+      releaseYear: 2030,
+    },
+    smartWatch: {
+      brand: "apple watch ",
+      model: "X-mel",
+    },
+  };
+
+  //
 }
